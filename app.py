ad6be113ac30ac9359bae7ee631b3b393a4dcc05
Shah Harsh Devangbhai <harshshah25102005@gmail.com>
2026-07-15 17:03:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -293,4 +293,3 @@ if __name__ == "__main__":
     # Vercel imports the 'app' object directly and does not call this.
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
-    
